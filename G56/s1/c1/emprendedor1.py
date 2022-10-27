a974d82b1fbeaa948b02767d1a6f16f8484252eb
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-10-27 20:41:20 -0300

    Formateado el documento

--- G56/s1/c1/emprendedor1.py
+++ G56/s1/c1/emprendedor1.py
@@ -1,15 +1,11 @@
 print('Bienvenido al calculador de utilidades! \n\n'
       'Debes ingresar el precio de suscripción, numero de usuarios y gastos totales.\n'
-      'EL PROGRAMA SÓLO FUNCIONARÁ SI ES QUE SE INGRESAN VALORES NUMÉRICOS.\n'
-      'SI SE INGRESAN VALORES NO NUMÉRICOS, ES ESPERABLE QUE EL PROGRAMA FALLE.\n\n')
+      'EL PROGRAMA SÓLO FUNCIONARÁ SI ES QUE SE INGRESAN VALORES NUMÉRICOS ENTEROS.\n'
+      'SI SE INGRESAN VALORES NO NUMÉRICOS ENTEROS, ES ESPERABLE QUE EL PROGRAMA FALLE.\n\n')
 
-precio_suscripcion = input('¿Cuál es el precio de suscripción?: ')
-numero_usuarios = input('¿Cuál es el número de usuarios?: ')
-gasto_total = input('¿Cuál es el gasto total?: ')
-
-precio_suscripcion = int(precio_suscripcion)
-numero_usuarios = int(numero_usuarios)
-gasto_total = int(gasto_total)
+precio_suscripcion = int(input('¿Cuál es el precio de suscripción?: '))
+numero_usuarios = int(input('¿Cuál es el número de usuarios?: '))
+gasto_total = int(input('¿Cuál es el gasto total?: '))
 
 utilidades = (precio_suscripcion * numero_usuarios) - gasto_total
 
--- G56/s1/c1/emprendedor2.py
+++ G56/s1/c1/emprendedor2.py
@@ -1,20 +1,18 @@
 print('Bienvenido al calculador de utilidades con usuarios Premium! \n\n'
-      'Debes ingresar el precio de suscripción, numero de usuarios y gastos totales.\n'
-      'EL PROGRAMA SÓLO FUNCIONARÁ SI ES QUE SE INGRESAN VALORES NUMÉRICOS.\n'
-      'SI SE INGRESAN VALORES NO NUMÉRICOS, ES ESPERABLE QUE EL PROGRAMA FALLE.\n\n')
+      'Debes ingresar el precio de suscripción, numero de usuarios normales, '
+      'numero de usuarios premium y gastos totales.\n'
+      'EL PROGRAMA SÓLO FUNCIONARÁ SI ES QUE SE INGRESAN VALORES NUMÉRICOS ENTEROS.\n'
+      'SI SE INGRESAN VALORES NO NUMÉRICOS ENTEROS, ES ESPERABLE QUE EL PROGRAMA FALLE.\n\n')
 
-precio_suscripcion = input('¿Cuál es el precio de suscripción?: ')
-numero_usuarios_normales = input('¿Cuál es el número de usuarios normales?: ')
-numero_usuarios_premium = input('¿Cuál es el número de usuarios premium?: ')
-gasto_total = input('¿Cuál es el gasto total?: ')
-
-precio_suscripcion = int(precio_suscripcion)
-numero_usuarios_normales = int(numero_usuarios_normales)
-numero_usuarios_premium = int(numero_usuarios_premium)
-gasto_total = int(gasto_total)
+precio_suscripcion = int(input('¿Cuál es el precio de suscripción?: '))
+numero_usuarios_normales = int(
+    input('¿Cuál es el número de usuarios normales?: '))
+numero_usuarios_premium = int(
+    input('¿Cuál es el número de usuarios premium?: '))
+gasto_total = int(input('¿Cuál es el gasto total?: '))
 
 utilidades = (((precio_suscripcion * numero_usuarios_normales)
                + (precio_suscripcion * 1.5 * numero_usuarios_premium))
               - gasto_total)
 
-print(f'Tus utilidades son: {utilidades}')
+print(f'Tus utilidades con los dos tipos de usaurios son: {utilidades}')
