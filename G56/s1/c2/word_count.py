c6d5ec18888d58268d26aef8941e64bf9760ec15
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-10-28 14:16:13 -0300

    Hecho word count

--- G56/s1/c2/word_count.py
+++ G56/s1/c2/word_count.py
@@ -1,5 +1,7 @@
 import sys
 
+from matplotlib.pyplot import text
+
 ruta_archivo_texto = sys.argv[1]
 
 with open(ruta_archivo_texto, 'r') as file:
@@ -7,11 +9,9 @@ with open(ruta_archivo_texto, 'r') as file:
 
 texto_separado_en_caracteres = list(texto)
 caracteres_unicos_texto = set(texto)
-cantidad_caracteres_unicos_texto = len(caracteres_unicos_texto)
+print(f'El número de caracteres distintos es: {len(caracteres_unicos_texto)}')
 
 palabras_separadas = texto.split(' ')
-cantidad_palabras_separadas = len(palabras_separadas)
+palabras_unicas = set(palabras_separadas)
 
-print(
-    f'El número de caracteres distintos es: {cantidad_caracteres_unicos_texto}')
-print(f'El número de palabras distintas es: {cantidad_palabras_separadas}')
+print(f'El número de palabras distintas es: {len(palabras_unicas)}')
