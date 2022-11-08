09ac229dcb98d14d81e432a1431e31b8e682e04b
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-08 19:49:53 -0300

    Incorporado clasificacion OMS y prints

--- G56/s2/c1/imc.py
+++ G56/s2/c1/imc.py
@@ -11,4 +11,23 @@ altura_en_m = altura_en_cm / 100
 
 imc = round(peso/(altura_en_m ** 2), 2)
 
+if imc < 18.5:
+    clasificacion = 'Bajo Peso'
+
+elif imc >= 18.5 and imc < 25:
+    clasificacion = 'Adecuado'
+
+elif imc >= 25 and imc < 30:
+    clasificacion = 'Sobrepeso'
+
+elif imc >= 30 and imc < 35:
+    clasificacion = 'Obesidad Grado I'
+
+elif imc >= 35 and imc < 40:
+    clasificacion = 'Obesidad Grado II'
+
+elif imc > 40:
+    clasificacion = 'Obesidad Grado III'
+
 print(f'Su IMC es {imc}')
+print(f'La clasificación OMS es {clasificacion}')
