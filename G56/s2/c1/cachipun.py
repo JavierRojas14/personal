8e10db7bb10eec802f78c13c2f06f2741b0ed2d2
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-10 19:23:58 -0300

    Re arreglado el codigo

--- G56/s2/c1/cachipun.py
+++ G56/s2/c1/cachipun.py
@@ -3,43 +3,44 @@ import random
 
 decisiones = ['piedra', 'papel', 'tijera']
 
-decision_usario = sys.argv[1]
-decision_computadora = random.choice(decisiones)
+decision_usuario = sys.argv[1]
+print(f'Tu jugaste {decision_usuario}')
 
-print(f'Tu jugaste {decision_usario}')
-print(f'Computador jugó {decision_computadora}')
+decision_usuario = decision_usuario.lower()
 
-decision_usario = decision_usario.lower()
+if decision_usuario in decisiones:
+    decision_computadora = random.choice(decisiones)
+    print(f'Computador jugó {decision_computadora}')
 
-if decision_usario == 'piedra':
-    if decision_computadora == 'piedra':
-        print('Hubo un empate!!')
+    if decision_usuario == 'piedra':
+        if decision_computadora == 'piedra':
+            print('Hubo un empate!!')
 
-    elif decision_computadora == 'papel':
-        print('Perdiste :(')
+        elif decision_computadora == 'papel':
+            print('Perdiste :(')
 
-    elif decision_computadora == 'tijera':
-        print('Ganaste!!')
+        elif decision_computadora == 'tijera':
+            print('Ganaste!!')
 
-elif decision_usario == 'papel':
-    if decision_computadora == 'piedra':
-        print('Ganaste!!')
+    elif decision_usuario == 'papel':
+        if decision_computadora == 'piedra':
+            print('Ganaste!!')
 
-    elif decision_computadora == 'papel':
-        print('Hubo un empate!!')
+        elif decision_computadora == 'papel':
+            print('Hubo un empate!!')
 
-    elif decision_computadora == 'tijera':
-        print('Perdiste :(')
+        elif decision_computadora == 'tijera':
+            print('Perdiste :(')
 
-elif decision_usario == 'tijera':
-    if decision_computadora == 'piedra':
-        print('Perdiste :(')
+    elif decision_usuario == 'tijera':
+        if decision_computadora == 'piedra':
+            print('Perdiste :(')
 
-    elif decision_computadora == 'papel':
-        print('Ganaste!!')
+        elif decision_computadora == 'papel':
+            print('Ganaste!!')
 
-    elif decision_computadora == 'tijera':
-        print('Hubo un empate!!')
+        elif decision_computadora == 'tijera':
+            print('Hubo un empate!!')
 
 else:
     print('Arguemento inválido: Debe ser piedra, papel o tijera')
