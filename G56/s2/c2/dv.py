bb38986dd5c2cdc965cad6dcf7d37f5cf203f1e0
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-10 20:20:16 -0300

    Se multiplica bien

--- G56/s2/c2/dv.py
+++ G56/s2/c2/dv.py
@@ -2,4 +2,21 @@ rut_sin_dv = input('Ingresa tu RUT sin puntos ni dígito verificador: ')
 # Ej 12345678
 
 serie_inicial = rut_sin_dv[1:-1]
+numero_a_multiplicar = rut_sin_dv[::-1]
 print(serie_inicial)
+print(numero_a_multiplicar)
+
+multiplicaciones = []
+i_serie = 0
+
+for numero_invertido in numero_a_multiplicar:
+    digito = int(serie_inicial[i_serie])
+    multiplicacion = int(numero_invertido) * digito
+    multiplicaciones.append(multiplicacion)
+
+    i_serie += 1
+    if i_serie > len(serie_inicial) - 1:
+        i_serie = 0
+
+print(multiplicaciones)
+
