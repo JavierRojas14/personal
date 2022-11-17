85b72f1244a72d3389af1aa9863b9cd9cd363f55
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-17 16:08:32 -0300

    Agregar filtro mayor/menos/otros

--- G56/s3/c1/filtro.py
+++ G56/s3/c1/filtro.py
@@ -7,14 +7,26 @@ precios = {'Notebook': 700000,
            'Escritorio': 135000,
            'Tarjeta de Video': 1500000}
 
+
 def filtrar_por_umbral(umbral, tipo='mayor'):
     if tipo == 'mayor':
-        filtrados = {llave: valor for llave, valor in precios.items() if valor >= umbral}
+        filtrados = {llave: valor for llave, valor in precios.items() if valor > umbral}
         articulos = list(filtrados.keys())
         print(f'Los productos mayores al umbral son: {", ".join(articulos)}')
 
+    elif tipo == 'menor':
+        filtrados = {llave: valor for llave, valor in precios.items() if valor < umbral}
+        articulos = list(filtrados.keys())
+        print(f'Los productos menores al umbral son: {", ".join(articulos)}')
 
+    else:
+        print('Lo sentimos, no es una operación válida')
 
 
 umbral = int(sys.argv[1])
-filtrar_por_umbral(umbral)
+if len(sys.argv) == 2:
+    filtrar_por_umbral(umbral)
+
+elif len(sys.argv) == 3:
+    tipo_calculo = sys.argv[2]
+    filtrar_por_umbral(umbral, tipo_calculo)
