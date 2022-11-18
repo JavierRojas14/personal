361402ac7de19ae74a557b43f22e726a497bf48b
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-18 12:05:47 -0300

    Linter

--- G56/s3/c1/filtro.py
+++ G56/s3/c1/filtro.py
@@ -1,13 +1,5 @@
 import sys
 
-precios = {'Notebook': 700000,
-           'Teclado': 25000,
-           'Mouse': 12000,
-           'Monitor': 250000,
-           'Escritorio': 135000,
-           'Tarjeta de Video': 1500000}
-
-
 def filtrar_por_umbral(umbral, tipo='mayor'):
     if tipo == 'mayor':
         filtrados = {llave: valor for llave, valor in precios.items() if valor > umbral}
@@ -22,6 +14,12 @@ def filtrar_por_umbral(umbral, tipo='mayor'):
     else:
         print('Lo sentimos, no es una operación válida')
 
+precios = {'Notebook': 700000,
+           'Teclado': 25000,
+           'Mouse': 12000,
+           'Monitor': 250000,
+           'Escritorio': 135000,
+           'Tarjeta de Video': 1500000}
 
 umbral = int(sys.argv[1])
 if len(sys.argv) == 2:
