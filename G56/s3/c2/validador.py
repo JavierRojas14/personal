65a7b1f47334d4a8a5587cf026bf339a34e28c4d
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-18 12:25:39 -0300

    Agregar funcionalidad validate

--- G56/s3/c2/validador.py
+++ G56/s3/c2/validador.py
@@ -2,18 +2,23 @@
 def validate(opciones, eleccion):
     # Definir validación de eleccion
     ##########################################################################
-    pass
-    
+    while True:
+        if eleccion not in opciones:
+            print(f'Su elección es inválida, por favor ingrese un valor que esté en {opciones}')
+            eleccion = input('Ingresa una Opción: ').lower()
+
+        elif eleccion in opciones:
+            break
+
+    print(f'Ingresó correctamente la elección: {eleccion}')
     ##########################################################################
     return eleccion
 
 
 if __name__ == '__main__':
-    
+
     eleccion = input('Ingresa una Opción: ').lower()
     # letras = ['a','b','c','d'] # pueden probar con letras también para verificar su funcionamiento.
-    numeros = ['0','1']
+    numeros = ['0', '1']
     # Si se ingresan valores no validos a eleccion debe seguir preguntando
     validate(numeros, eleccion)
-    
-    
