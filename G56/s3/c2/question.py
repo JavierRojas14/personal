26e0b5a98ae402692a9fb915b58c81450a134560
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-18 12:20:12 -0300

    Agregar archivos de apoyo

---
+++ G56/s3/c2/question.py
@@ -0,0 +1,42 @@
+import preguntas as p
+import random
+from shuffle import shuffle_alt
+
+# Opciones dadas para escoger.
+###############################################
+opciones = {'basicas': [1,2,3],
+            'intermedias': [1,2,3],
+            'avanzadas': [1,2,3]}
+###############################################
+
+def choose_q(dificultad):
+    #escoger preguntas por dificultad
+    preguntas = 
+    
+    # usar opciones desde ambiente global
+    global 
+    # escoger una pregunta
+    n_elegido = 
+    # eliminarla del ambiente global para no escogerla de nuevo
+    
+    
+    # escoger enunciado y alternativas mezcladas
+    pregunta = 
+    alternativas = 
+    
+    
+    return pregunta['enunciado'], alternativas
+
+if __name__ == '__main__':
+    # si ejecuto el programa, las preguntas cambian de orden, pero nunca debieran repetirse
+    pregunta, alternativas = choose_q('basicas')
+    print(f'El enunciado es: {pregunta}')
+    print(f'Las alternativas son: {alternativas}')
+    
+    pregunta, alternativas = choose_q('basicas')
+    print(f'El enunciado es: {pregunta}')
+    print(f'Las alternativas son: {alternativas}')
+    
+    pregunta, alternativas = choose_q('basicas')
+    print(f'El enunciado es: {pregunta}')
+    print(f'Las alternativas son: {alternativas}')
---
+++ G56/s3/c2/validador.py
@@ -0,0 +1,19 @@
+
+def validate(opciones, eleccion):
+    # Definir validación de eleccion
+    ##########################################################################
+    pass
+    
+    ##########################################################################
+    return eleccion
+
+
+if __name__ == '__main__':
+    
+    eleccion = input('Ingresa una Opción: ').lower()
+    # letras = ['a','b','c','d'] # pueden probar con letras también para verificar su funcionamiento.
+    numeros = ['0','1']
+    # Si se ingresan valores no validos a eleccion debe seguir preguntando
+    validate(numeros, eleccion)
+    
+    
