5af55ec689b69bf4c861880433c98ec5d65acec3
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-20 18:33:24 -0300

    Agregar validador de numero preguntas por nivel

--- G56/s3/c2/main.py
+++ G56/s3/c2/main.py
@@ -45,7 +45,7 @@ while correcto and n_pregunta < 3*p_level:
     if n_pregunta == 0:
         p_level = input('¿Cuántas preguntas por nivel? (Máximo 3): ')
         # 3. Validar el número de preguntas por nivel
-        p_level = 
+        p_level = validate(['1', '2', '3', p_level])
         
     if continuar == 'y':
         #contador de preguntas
