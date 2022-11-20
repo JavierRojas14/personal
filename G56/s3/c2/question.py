bf268bb61bcb1a6a675fcc90a8d34b3b8957dae9
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-20 17:59:40 -0300

    Elige aleatoriamente preguntas/alternativas

--- G56/s3/c2/question.py
+++ G56/s3/c2/question.py
@@ -11,24 +11,21 @@ opciones = {'basicas': [1,2,3],
 
 def choose_q(dificultad):
     #escoger preguntas por dificultad
-    pregunta_escogida = preguntas[dificultad]
-    print(preguntas)
+    preguntas_dificultad = p.pool_preguntas[dificultad]
     
     # usar opciones desde ambiente global
     global opciones
     # escoger una pregunta
     opciones_disponibles = opciones[dificultad]
-    n_elegido = random.randrange((len(opciones_disponibles)))
-    print(n_elegido)
+    indice_elegido = random.randrange((len(opciones_disponibles)))
+    n_elegido = opciones_disponibles[indice_elegido]
     # eliminarla del ambiente global para no escogerla de nuevo
-    opciones[dificultad].pop(n_elegido)
-    print(opciones)
-    
+    opciones[dificultad].pop(indice_elegido)
     
     # escoger enunciado y alternativas mezcladas
-    llave_pregunta = preguntas.keys()[n_elegido]
-    pregunta = preguntas[llave_pregunta]
-    alternativas = shuffle_alt(pregunta['alternativas'])
+    llave_pregunta = list(preguntas_dificultad.keys())[n_elegido - 1]
+    pregunta = preguntas_dificultad[llave_pregunta]
+    alternativas = shuffle_alt(pregunta)
     
     
     return pregunta['enunciado'], alternativas
@@ -37,12 +34,12 @@ if __name__ == '__main__':
     # si ejecuto el programa, las preguntas cambian de orden, pero nunca debieran repetirse
     pregunta, alternativas = choose_q('basicas')
     print(f'El enunciado es: {pregunta}')
-    print(f'Las alternativas son: {alternativas}')
+    print(f'Las alternativas son: {alternativas}\n')
     
     pregunta, alternativas = choose_q('basicas')
     print(f'El enunciado es: {pregunta}')
-    print(f'Las alternativas son: {alternativas}')
+    print(f'Las alternativas son: {alternativas}\n')
     
     pregunta, alternativas = choose_q('basicas')
     print(f'El enunciado es: {pregunta}')
-    print(f'Las alternativas son: {alternativas}')
+    print(f'Las alternativas son: {alternativas}\n')
