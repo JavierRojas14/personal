ad0cca62618acea2935ce98b9dbb1b98ba17e395
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-20 19:05:56 -0300

    Cambiar preguntas para la trivia

--- G56/s3/c2/preguntas.py
+++ G56/s3/c2/preguntas.py
@@ -1,39 +1,40 @@
 preguntas_basicas = {
-    'pregunta_1': {'enunciado':['Enunciado básico 1'],
-    'alternativas': [['alt_1', 0], 
-                     ['alt_2', 1], 
-                     ['alt_3', 0], 
-                     ['alt_4', 0]]},
-    'pregunta_2': {'enunciado':['Enunciado básico 2'],
-    'alternativas': [['alt_1', 0], 
-                     ['alt_2', 1], 
-                     ['alt_3', 0], 
-                     ['alt_4', 0]]},
+    'pregunta_1': {'enunciado':['¿Cuánto es 1 + 1?'],
+    'alternativas': [['1', 0], 
+                     ['2', 1], 
+                     ['3', 0], 
+                     ['4', 0]]},
+    'pregunta_2': {'enunciado':['¿Cuánto es 2 + 1?'],
+    'alternativas': [['3', 1], 
+                     ['49', 0], 
+                     ['soy', 0], 
+                     ['asd', 0]]},
     
-'pregunta_3': {'enunciado':['Enunciado básico 3'],
-    'alternativas': [['alt_1', 0], 
-                     ['alt_2', 1], 
-                     ['alt_3', 0], 
-                     ['alt_4', 0]]}
+'pregunta_3': {'enunciado':['¿De qué color es el cielo?'],
+    'alternativas': [['Azul', 1], 
+                     ['Verde', 0], 
+                     ['Morado', 0], 
+                     ['Plata', 0]]}
 }
 
 preguntas_intermedias = {
-    'pregunta_1': {'enunciado':['Enunciado intermedio 1'],
-    'alternativas': [['alt_1', 0], 
-                     ['alt_2', 1], 
-                     ['alt_3', 0], 
-                     ['alt_4', 0]]},
-    'pregunta_2': {'enunciado':['Enunciado intermedio 2'],
-    'alternativas': [['alt_1', 0], 
-                     ['alt_2', 1], 
-                     ['alt_3', 0], 
-                     ['alt_4', 0]]},
+    'pregunta_1': {'enunciado':['La refracción de la luz consiste en: '],
+    'alternativas': [['Cómo se comporta la luz cuando choca con una superficie', 0], 
+                     ['La energía que lleva la luz', 0], 
+                     ['Las longitudes de onda que componen la luz', 0], 
+                     ['El cambio de ángulo de la luz al entrar a otro medio', 1]]},
+
+    'pregunta_2': {'enunciado':['¿De qué color es el cielo?'],
+    'alternativas': [['Azul', 0], 
+                     ['Celeste', 1], 
+                     ['Azul-marino', 0], 
+                     ['Acuamarino', 0]]},
     
-'pregunta_3': {'enunciado':['Enunciado intermedio 3'],
-    'alternativas': [['alt_1', 0], 
-                     ['alt_2', 1], 
-                     ['alt_3', 0], 
-                     ['alt_4', 0]]}
+'pregunta_3': {'enunciado':['¿Cuánto es la hipotenusa de un triangulo 1x1?'],
+    'alternativas': [['sqrt(2)', 1], 
+                     ['2', 0], 
+                     ['raiz cubica de 2', 0], 
+                     ['4', 0]]}
 }
 
 preguntas_avanzadas = {
