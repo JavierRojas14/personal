bdc65c267b5ca4247c44e6e52bed38373a1868b2
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-20 18:46:36 -0300

    Agregar elección de preguntas por nivel

--- G56/s3/c2/level.py
+++ G56/s3/c2/level.py
@@ -2,8 +2,25 @@ def choose_level(n_pregunta, p_level):
     
     # Construir lógica para escoger el nivel
     ##################################################
-    pass
-    
+    if p_level == 2:
+        if n_pregunta >= 1 and n_pregunta <= 2:
+            level = 'basicas'
+        
+        elif n_pregunta >= 3 and n_pregunta <= 4:
+            level = 'intermedias'
+        
+        elif n_pregunta >= 5:
+            level = 'avanzadas'
+        
+    elif p_level == 3:
+        if n_pregunta >= 1 and n_pregunta <= 3:
+            level = 'basicas'
+        
+        elif n_pregunta >= 4 and n_pregunta <= 6:
+            level = 'intermedias'
+        
+        elif n_pregunta >= 7:
+            level = 'avanzadas'
     
     ##################################################
     
