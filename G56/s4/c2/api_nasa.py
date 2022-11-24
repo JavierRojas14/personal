182b8cd4d4140c9b21299daf51464f4f9674cd7f
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-24 19:42:12 -0300

    Cambiar request a latest, y dejar solo los primeros 25

--- G56/s4/c2/api_nasa.py
+++ G56/s4/c2/api_nasa.py
@@ -8,15 +8,17 @@ def pretty_print_request(respuesta):
 
 token = 'He6gOY5Zhgh8nSzdGHEhI9st5tndoP5XQtMpswwN'
 
-url_25_fotos = f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-12-27&api_key={token}'
-response_25_fotos = requests.request('GET', url=url_25_fotos)
-pretty_print_request(response_25_fotos)
+url = f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key={token}'
+response = requests.request('GET', url=url)
+pretty_print_request(response)
 
 
-diccionario_respuesta = json.loads(response_25_fotos.text)
+diccionario_respuesta = json.loads(response.text)
+ultimas_fotos = diccionario_respuesta['latest_photos']
+ultimas_fotos = ultimas_fotos[:25]
 
 lista_urls = []
-for fotos in diccionario_respuesta['photos']:
+for fotos in diccionario_respuesta['latest_photos']:
     lista_urls.append(fotos['img_src'])
 
 print(lista_urls)
