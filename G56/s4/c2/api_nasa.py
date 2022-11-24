599328a125f36716c5c313df70b7a45e803087ec
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-24 19:24:20 -0300

    Crear funcion para hacer print bonito de las respuestas json

--- G56/s4/c2/api_nasa.py
+++ G56/s4/c2/api_nasa.py
@@ -1,10 +1,13 @@
 import requests
 import json
 
+def pretty_print_request(respuesta):
+    print(json.dumps(json.loads(respuesta.text), indent=1))
+
 token = 'He6gOY5Zhgh8nSzdGHEhI9st5tndoP5XQtMpswwN'
 
 url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos'
 url_token = f'{url}?api_key={token}'
 
 response = requests.request('GET', url=url_token)
-print(json.dumps(json.loads(response.text), indent=1))
+pretty_print_request(response)
