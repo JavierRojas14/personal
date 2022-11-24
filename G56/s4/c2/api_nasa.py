cc9e54a444d716fa09f334697c1ea59804bbd77b
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-24 19:54:34 -0300

    Crea lista con los multiples li posibles

--- G56/s4/c2/api_nasa.py
+++ G56/s4/c2/api_nasa.py
@@ -5,6 +5,31 @@ import json
 def pretty_print_request(respuesta):
     print(json.dumps(json.loads(respuesta.text), indent=1))
 
+def build_web_page(lista_urls):
+
+    li_items = []
+    lista_urls.append('soy')
+    for url in lista_urls:
+        objeto_li = f'  <li><img src="{url}"></li>'
+        li_items.append(objeto_li)
+    
+    li_items_str = '\n'.join(li_items)
+
+
+    documento = f'''
+    <html>
+    <head>
+    </head>
+    <body>
+    <ul>
+    {li_items_str}
+    </ul>
+    </body>
+    </head>
+    </html>
+    '''
+    print(documento)
+
 
 token = 'He6gOY5Zhgh8nSzdGHEhI9st5tndoP5XQtMpswwN'
 
@@ -22,3 +47,5 @@ for fotos in diccionario_respuesta['latest_photos']:
     lista_urls.append(fotos['img_src'])
 
 print(lista_urls)
+
+build_web_page(lista_urls=lista_urls)
