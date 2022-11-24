33c0634fe0e1c0bf5e65f13eb9929e900617a659
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-24 19:33:29 -0300

    Crea lista de urls de las imagenes

--- G56/s4/c2/api_nasa.py
+++ G56/s4/c2/api_nasa.py
@@ -11,3 +11,12 @@ token = 'He6gOY5Zhgh8nSzdGHEhI9st5tndoP5XQtMpswwN'
 url_25_fotos = f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-12-27&api_key={token}'
 response_25_fotos = requests.request('GET', url=url_25_fotos)
 pretty_print_request(response_25_fotos)
+
+
+diccionario_respuesta = json.loads(response_25_fotos.text)
+
+lista_urls = []
+for fotos in diccionario_respuesta['photos']:
+    lista_urls.append(fotos['img_src'])
+
+print(lista_urls)
