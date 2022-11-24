0418599aca732655b660a0dffe60546198b99371
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-24 19:30:27 -0300

    Corregir request de las 25 fotos

--- G56/s4/c2/api_nasa.py
+++ G56/s4/c2/api_nasa.py
@@ -1,13 +1,13 @@
 import requests
 import json
 
+
 def pretty_print_request(respuesta):
     print(json.dumps(json.loads(respuesta.text), indent=1))
 
+
 token = 'He6gOY5Zhgh8nSzdGHEhI9st5tndoP5XQtMpswwN'
 
-url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos'
-url_token = f'{url}?api_key={token}'
-
-response = requests.request('GET', url=url_token)
-pretty_print_request(response)
+url_25_fotos = f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-12-27&api_key={token}'
+response_25_fotos = requests.request('GET', url=url_25_fotos)
+pretty_print_request(response_25_fotos)
