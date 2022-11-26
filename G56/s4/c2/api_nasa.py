3d2aec3b83142a8c501c8207e24d379e7e5bb23c
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-26 15:22:39 -0300

    Ordena el código

--- G56/s4/c2/api_nasa.py
+++ G56/s4/c2/api_nasa.py
@@ -5,15 +5,15 @@ import json
 def pretty_print_request(respuesta):
     print(json.dumps(json.loads(respuesta.text), indent=1))
 
+
 def build_web_page(lista_urls):
 
     li_items = []
     for url in lista_urls:
         objeto_li = f'  <li><img src="{url}"></li>'
         li_items.append(objeto_li)
-    
-    li_items_str = '\n'.join(li_items)
 
+    li_items_str = '\n'.join(li_items)
 
     documento = f'''
 <html>
@@ -34,14 +34,13 @@ token = 'He6gOY5Zhgh8nSzdGHEhI9st5tndoP5XQtMpswwN'
 
 url = f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key={token}'
 response = requests.request('GET', url=url)
-pretty_print_request(response)
+pretty_print_request(respuesta=response)
 
 
 diccionario_respuesta = json.loads(response.text)
+# Primer punto de la tarea. Quedarse con los primeros 25 registros.
 ultimas_fotos = diccionario_respuesta['latest_photos'][:25]
+# Segundo punto de la tarea. Hacer una lista con los urls
 lista_urls = [fotos['img_src'] for fotos in ultimas_fotos]
 
 web_page = build_web_page(lista_urls=lista_urls)
-
-with open('index.html', 'w') as file:
-    file.write(web_page)
