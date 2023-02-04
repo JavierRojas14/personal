1fcbcb02a25f369b1486a16bf4e24511d488fded
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-02-04 13:26:19 -0300

    Crea funcion para cv con roc

--- G56/fundamentos-data-science/hito_4/funciones_auxiliares_hito_4.py
+++ G56/fundamentos-data-science/hito_4/funciones_auxiliares_hito_4.py
@@ -17,3 +17,18 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import roc_curve, roc_auc_score
 
+def separar_vector_objetivo_e_indep(df, vector_objetivo):
+    X = df.drop(columns='income')
+    y = df['income']
+
+    return X, y
+
+def calcular_roc_cross_val(df, vector_objetivo, cantidad_folds):
+    X, y = separar_vector_objetivo_e_indep(df, vector_objetivo)
+    X_escalado = StandardScaler().fit_transform(X)
+
+    roc_acumulado = cross_val_score(LogisticRegression(), X=X_escalado, y=y, cv=cantidad_folds, 
+                                    scoring='roc_auc')
+    
+    return roc_acumulado
+
