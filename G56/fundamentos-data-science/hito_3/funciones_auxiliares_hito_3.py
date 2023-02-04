804ea6b513c4397ef26d0d215d8afef83b547068
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-02-04 13:12:38 -0300

    Unifica creacion de modelo logistico

--- G56/fundamentos-data-science/hito_3/funciones_auxiliares_hito_3.py
+++ G56/fundamentos-data-science/hito_3/funciones_auxiliares_hito_3.py
@@ -17,6 +17,17 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import roc_curve, roc_auc_score
 
+def crear_formula_econometria(df, vector_objetivo):
+    variables_independientes = ' + '.join(df.drop(columns=vector_objetivo).columns)
+    formula_final = f'income ~ {variables_independientes}'
+
+    return formula_final
+
+def crear_modelo_logistico(df, vector_objetivo):
+    formula = crear_formula_econometria(df, vector_objetivo)
+    modelo = smf.logit(formula, df).fit()
+
+    return modelo
 
 
 
