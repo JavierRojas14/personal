82172be3647030b247f3c9d4254ec9219069b861
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-02-04 13:16:37 -0300

    Depura funcionales aux hito_4

---
+++ G56/fundamentos-data-science/hito_4/funciones_auxiliares_hito_4.py
@@ -0,0 +1,19 @@
+import warnings
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import missingno as msno
+
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_curve, roc_auc_score
+
