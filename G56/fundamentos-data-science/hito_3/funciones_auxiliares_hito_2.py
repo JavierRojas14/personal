c0dca92ba6f148e939f67a8d902d7e45f8cc4494
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-02-06 12:12:41 -0300

    Modifica archivo funciones auxs

--- G56/fundamentos-data-science/hito_3/funciones_auxiliares_hito_2.py
+++ G56/fundamentos-data-science/hito_3/funciones_auxiliares_hito_2.py
@@ -1,5 +1,3 @@
-import warnings
-
 import pandas as pd
 import numpy as np
 
@@ -7,16 +5,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 import missingno as msno
-
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
-
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import roc_curve, roc_auc_score
-
 
 REEMPLAZO_OCCUPATION = {
     'white-collar': ['Prof-specialty', 'Exec-managerial', 'Adm-clerical', 'Sales', 'Tech-support'],
@@ -55,13 +43,24 @@ REEMPLAZO_COUNTRY = {'america': ["United-States", "Mexico", "Puerto-Rico", "Cana
                      'africa': ["South"]}
 
 REEMPLAZO_INCOME = {0: ['<=50K'], 1: ['>50K']}
-REEMPLAZO_SEX = {0: ['Male'], 1: ['Female']}
+REEMPLAZO_GENDER = {0: ['Male'], 1: ['Female']}
 
-RECODIFICACION_ENUNCIADO_1 = [REEMPLAZO_OCCUPATION, REEMPLAZO_WORKCLASS, REEMPLAZO_EDUCATION,
-                              REEMPLAZO_MARITAL, REEMPLAZO_COUNTRY, REEMPLAZO_INCOME, REEMPLAZO_SEX]
+CAMBIO_HITO_1 = {'occupation': REEMPLAZO_OCCUPATION,
+                 'workclass': REEMPLAZO_WORKCLASS,
+                 'education': REEMPLAZO_EDUCATION,
+                 'marital-status': REEMPLAZO_MARITAL,
+                 'native-country': REEMPLAZO_COUNTRY,
+                 'income': REEMPLAZO_INCOME,
+                 'gender': REEMPLAZO_GENDER}
 
 
 def analizar_vector_objetivo_discreto(serie_variable):
+    '''Esta función permite ver la frecuencia, distribución y cantidad de valores faltantes
+    de un vector objetivo discreto
+
+    :param serie_variable: Es el pandas Series del vector objetivo discreto
+    :type serie_variable: pd.Series
+    '''
     frecuencias = serie_variable.value_counts()
     porcentajes = serie_variable.value_counts('%')
     total = pd.DataFrame({'Frecuencia': frecuencias,
@@ -76,12 +75,32 @@ def analizar_vector_objetivo_discreto(serie_variable):
 
 
 def separar_df_a_numericas_categoricas(df):
+    '''Esta funcion permite separar un DataFrame en sus variables categoricas (que están en formato
+    string u object) y las variables numéricas (que estén en algún formato numérico como int o
+    float)
+
+    :param df: Es el DataFrame que se quiere separar
+    :type df: pd.DataFrame
+
+    :returns: Una tupla con las variables numericas y categoricas
+    :rtype: tuple
+    '''
     numericas = df.select_dtypes('number')
     categoricas = df.select_dtypes('object')
     return numericas, categoricas
 
 
 def graficar_distribucion_variable_numerica(serie_numerica, nombre_grafico):
+    '''Esta funcion permite graficar la distribución de una variable numérica con un histograma
+    y un gráfico de cajas y bigotes. Además, muestra la media en el histograma
+
+    :param serie_numerica: Es la Serie o el conjunto de datos de la variable numérica que se quiere
+    graficar
+    :type serie_numerica: pd.Series
+
+    :param nombre_grafico: Es el nombre que se le quiere poner al gráfico
+    :type nombre_grafico: str
+    '''
     fig, axis = plt.subplots(1, 2)
     sns.histplot(data=serie_numerica, ax=axis[0])
     axis[0].axvline(serie_numerica.mean(), color='tomato')
@@ -92,6 +111,12 @@ def graficar_distribucion_variable_numerica(serie_numerica, nombre_grafico):
 
 
 def analizar_distr_todas_las_variables_numericas(df_numericas):
+    '''Función que permite iterar en todas las variables numéricas de un DataFrame para graficar
+    su distribución. Además, muestra las medidas de tendencia central.
+
+    :param df_numericas: Es el DataFrame que contiene todas las variables numéricas a analizar
+    :type df_numericas: pd.DataFrame
+    '''
     print('Analizando todas las variables numericas \n')
     display(df_numericas.describe())
 
@@ -101,6 +126,16 @@ def analizar_distr_todas_las_variables_numericas(df_numericas):
 
 
 def graficar_distribucion_variable_categorica(serie_categorica, nombre_grafico):
+    '''Funcion que permite analizar la distribución y frecuencia de cada categoria dentro de una
+    variable categorica. Además, genera un gráfico de barras con la frecuencia y el valor de la
+    categoria en la variable. Este gráfico esta ordenado descendientemente.
+
+    :param serie_categorica: Es la pd.Series o conjunto de datos categoricos que se quieren analizar
+    :type serie_categorica: pd.Series
+
+    :param nombre_grafico: Es el título que se le quiere poner al gráfico
+    :type nombre_grafico: str
+    '''
     serie_conteo = serie_categorica.value_counts()
     print(nombre_grafico)
     print(serie_conteo)
@@ -111,6 +146,12 @@ def graficar_distribucion_variable_categorica(serie_categorica, nombre_grafico):
 
 
 def analizar_dist_todas_las_variables_categoricas(df_categoricas):
+    '''Función que permite iterar en todas las variables categóricas de un DataFrame para graficar
+    su distribución.
+
+    :param df_numericas: Es el DataFrame que contiene todas las variables categóricas a analizar
+    :type df_numericas: pd.DataFrame
+    '''
     print('Analizando todas las variables categoricas \n')
     for columna_categorica, serie_categorica in df_categoricas.items():
         graficar_distribucion_variable_categorica(
@@ -118,8 +159,16 @@ def analizar_dist_todas_las_variables_categoricas(df_categoricas):
 
 
 def analizar_valores_faltantes(variables_a_analizar):
+    '''Función que permite cuantificar la cantidad de valores faltantes en un DataFrame que se
+    quiera analizar. Muestra la cantidad y el porcentaje de valores faltantes por columna/variable
+    presente en la base de datos
+
+    :param variables_a_analizar: Es el conjunto de variables que se quieren analizar
+    :type variables_a_analizar: pd.DataFrame
+    '''
     valores_faltantes = variables_a_analizar.isnull().sum()
-    porcentaje_faltantes = round(valores_faltantes * 100 / len(variables_a_analizar), 2)
+    porcentaje_faltantes = round(
+        valores_faltantes * 100 / len(variables_a_analizar), 2)
 
     faltantes_resumen = pd.DataFrame({'columna': variables_a_analizar.columns,
                                       'cantidad_na': valores_faltantes,
@@ -130,16 +179,34 @@ def analizar_valores_faltantes(variables_a_analizar):
 
 
 def analizar_variables_indpendientes(df_independientes):
-    numericas, categoricas = separar_df_a_numericas_categoricas(df_independientes)
+    '''Función que permite analizar las variables independientes elegidas para un modelo de
+    predicción. Esta función clasifica y separa las variables en numéricas y categóricas. Luego,
+    analiza y grafica la distribución de ambos tipos de variables y finalmente analiza la cantidad
+    de valores faltantes.
+
+    :param df_independientes: Es la base de datos que contiene todas las variables independientes
+    a ser utilizadas en el modelo predictivo
+    :type df_independientes: pd.DataFrame
+    '''
+    numericas, categoricas = separar_df_a_numericas_categoricas(
+        df_independientes)
     analizar_distr_todas_las_variables_numericas(numericas)
     analizar_dist_todas_las_variables_categoricas(categoricas)
     analizar_valores_faltantes(df_independientes)
 
 
 def mostrar_perdida_de_datos(df_completa):
+    '''Función que cuantifica la cantidad y el porcentaje de datos perdidos cuando se utiliza
+    .dropna() en una base de datos de forma global.
+
+    :param df_completa: Base de datos que se quiere saber cuánto cambia cuando se utiliza
+    .dropna() de forma global.
+    :type df_completa: pd.DataFrame
+    '''
     cantidad_valores_originales = len(df_completa)
     cantidad_valores_droppeados = len(df_completa.dropna())
-    porcentaje_droppeo = (cantidad_valores_droppeados / cantidad_valores_originales)
+    porcentaje_droppeo = (cantidad_valores_droppeados /
+                          cantidad_valores_originales)
     cambio = round((1 - porcentaje_droppeo) * 100, 2)
 
     print(f'Al droppear todos los valores faltantes en la DataFrame se pierde el {cambio}% '
@@ -147,37 +214,97 @@ def mostrar_perdida_de_datos(df_completa):
 
 
 def analizar_correlacion_todas_las_variables(df_variables):
+    '''Funcion que calcula y grafica la correlacion entre variables en una base de datos
+
+    :param df_variables: Es la base de datos que contiene las variables a analizar
+    :type df_variables: pd.DataFrame
+    '''
     corr = df_variables.corr()
     sns.heatmap(corr, cmap='Blues', annot=True)
     plt.show()
 
 
-def recodificar_variable(df, diccionario_cambio):
-    tmp = df.copy()
+def cambiar_valores_en_variable(serie, diccionario_cambio):
+    '''Funcion que permite reemplazar los valores de una serie basado en un diccionario. El
+    diccionario de cambio tiene el formato {valor_nuevo, valores_a_reemplazar}
+
+    :param serie: Conjunto de datos que se les quiere cambiar/reemplazar alguna imputación
+    :type serie: pd.Series
+
+    :param diccionario_cambio: Diccionario que especifica el valor nuevo que se quiere ingresar
+    en la serie de datos, y los valores antiguos a reemplazar
+
+    :returns: Una copia del conjunto de datos con los valores reemplazados según el diccionario
+    :rtype: pd.Series
+    '''
+    tmp = serie.copy()
     for valor_nuevo, valores_antiguos in diccionario_cambio.items():
         tmp = tmp.replace(valores_antiguos, valor_nuevo)
 
     return tmp
 
 
-def recodificar_enunciado_uno(df):
-    recodificada = df.copy()
-    for dict_recod in RECODIFICACION_ENUNCIADO_1:
-        recodificada = recodificar_variable(recodificada, dict_recod)
+def recodificar_variables(df, diccionario_var_a_cambiar):
+    '''Funcion que itera en un diccionario de variables a cambiar dentro de un DataFrame. Luego,
+    reemplaza los valores presentes en cada variable según lo indicado en el diccionario presente
+    para cada variable.
 
-    return recodificada
+    :param df: Es el DataFrame que contiene las variables que quieren ser recodificadas
+    :type df: pd.DataFrame
+
+    :param diccionario_var_a_cambiar: Es un diccionario que contiene el nombre de la variable a
+    cambiar como llave, y que tiene como valor el diccionario de cambio específico para la variable.
+    :type diccionario_var_a_cambiar: dict
+
+    :returns: El DataFrame con todas las variables estipuladas en diccionario_var_a_cambiar
+    modificadas
+    :rtype: pd.DataFrame
+    '''
+    tmp = df.copy()
+
+    for variable, dict_reemplazo in diccionario_var_a_cambiar.items():
+        tmp[variable] = cambiar_valores_en_variable(
+            tmp[variable], dict_reemplazo)
+
+    return tmp
 
 
 def codificar_a_one_hot(df, nombre_columna, serie_columna):
+    '''Funcion que codifica una variable categorica no binaria a formato Leave-One-Out Encoding.
+    Ademas, une esta codificacion al DataFrame y elimina la variable original.
+
+    :param df: Es el DataFrame que contiene la variable que se quiere codificar
+    :type df: pd.DataFrame
+
+    :param nombre_columna: Es el nombre de la variable que se quiere codificar
+    :type nombre_columna: str
+
+    :param serie_columna: Es el conjunto de datos de la variable que se quiere codificar
+    :type serie_columna: pd.Series
+
+    :returns: Retorna el DataFrame con la variable codificada según el formato Leave-One-Out Encoding.
+    Además, sin la variable original
+    :rtype: pd.DataFrame
+    '''
     tmp = df.copy()
 
-    tmp = tmp.join(pd.get_dummies(serie_columna, drop_first=True), rsuffix=f'_{nombre_columna}')
+    tmp = tmp.join(pd.get_dummies(serie_columna, drop_first=True),
+                   rsuffix=f'_{nombre_columna}')
     tmp = tmp.drop(columns=nombre_columna)
 
     return tmp
 
 
 def unir_codificacion_one_hot_vars_categoricas(df):
+    '''Función que itera en todas las variables categóricas presentes en un DataFrame para llevarlas
+    a formato Leave-One-Out Encoding.
+
+    :param df: Es el DataFrame que contiene nuestras variables categoricas a codificar
+    :type df: pd.DataFrame
+
+    :returns: El DataFrame con todas las variables categóricas codificadas a Leave-One-Out Encoding
+    :rtype: pd.DataFrame
+    '''
     tmp = df.copy()
 
     _, categoricas = separar_df_a_numericas_categoricas(df)
@@ -186,52 +313,34 @@ def unir_codificacion_one_hot_vars_categoricas(df):
 
     return tmp
 
+
 def preprocesar_y_recodificar_enunciado_uno(df):
+    '''Función específica que engloba el preprocesamiento y trata de valores faltantes para el
+    enunciado 1 de la prueba de Fundamentos de Data Science de Desafios Latam.
+
+    :param df: DataFrame sin modificar del enunciado 1 prueba de Fundamentos de Data Science 
+    de Desafios Latam
+    :type df: pd.DataFrame
+
+    :returns: El DataFrame completamente preprocesado y sin valores faltantes
+    :rtype: pd.DataFrame
+    '''
     tmp = df.copy()
 
     tmp = tmp.replace('?', np.nan)
     tmp = tmp.dropna()
-    tmp = recodificar_enunciado_uno(tmp)
+    tmp = tmp.drop(columns='educational-num')
+    tmp = recodificar_variables(tmp, CAMBIO_HITO_1)
     tmp = unir_codificacion_one_hot_vars_categoricas(tmp)
     tmp.columns = tmp.columns.str.replace('-', '_')
 
     return tmp
 
-############################ Enunciado 2
+# Enunciado 2
 
-def corregir_var_numerica_con_comillas(serie_semi_numerica):
-    return serie_semi_numerica.str.replace('"', '').astype(float)
-
-def corregir_variables_numericas(df):
-    tmp = df.copy()
-
-    vars_erroneas = ['age', 'goout', 'health']
-    for variable_erronea in vars_erroneas:
-        tmp[variable_erronea] = corregir_var_numerica_con_comillas(tmp[variable_erronea])
-
-    variables_numericas = ['Dalc',
-                       'Fedu',
-                       'G1',
-                       'G2',
-                       'G3',
-                       'Medu',
-                       'Walc',
-                       'absences',
-                       'age',
-                       'failures',
-                       'famrel',
-                       'freetime',
-                       'goout',
-                       'health',
-                       'studytime',
-                       'traveltime']
-
-    tmp[variables_numericas] = tmp[variables_numericas].astype(float)
-
-    return tmp
 
 REEMPLAZO_SCHOOL = {0: ['GP'], 1: ['MS']}
-REEMPLAZO_SEX = {0: ['F'], 1: ['M']}
+REEMPLAZO_GENDER = {0: ['F'], 1: ['M']}
 REEMPLAZO_ADDRESS = {0: ['U'], 1: ['R']}
 REEMPLAZO_FAMSIZE = {0: ['GT3'], 1: ['LE3']}
 REEMPLAZO_PSTATUS = {0: ['T'], 1: ['A']}
@@ -245,7 +354,7 @@ REEMPLAZO_INTERNET = {0: ['yes'], 1: ['no']}
 REEMPLAZO_ROMANTIC = {0: ['no'], 1: ['yes']}
 
 CAMBIO_HITO_2 = {'school': REEMPLAZO_SCHOOL,
-                 'sex': REEMPLAZO_SEX,
+                 'sex': REEMPLAZO_GENDER,
                  'address': REEMPLAZO_ADDRESS,
                  'famsize': REEMPLAZO_FAMSIZE,
                  'Pstatus': REEMPLAZO_PSTATUS,
@@ -258,23 +367,57 @@ CAMBIO_HITO_2 = {'school': REEMPLAZO_SCHOOL,
                  'internet': REEMPLAZO_INTERNET,
                  'romantic': REEMPLAZO_ROMANTIC}
 
+
+def corregir_var_numerica_con_comillas(serie_semi_numerica):
+    return serie_semi_numerica.str.replace('"', '').astype(float)
+
+
+def corregir_variables_numericas(df):
+    tmp = df.copy()
+
+    vars_erroneas = ['age', 'goout', 'health']
+    for variable_erronea in vars_erroneas:
+        tmp[variable_erronea] = corregir_var_numerica_con_comillas(
+            tmp[variable_erronea])
+
+    variables_numericas = ['Dalc',
+                           'Fedu',
+                           'G1',
+                           'G2',
+                           'G3',
+                           'Medu',
+                           'Walc',
+                           'absences',
+                           'age',
+                           'failures',
+                           'famrel',
+                           'freetime',
+                           'goout',
+                           'health',
+                           'studytime',
+                           'traveltime']
+
+    tmp[variables_numericas] = tmp[variables_numericas].astype(float)
+
+    return tmp
+
+
 def cambiar_vars_binarias_enunciado_dos(df):
     tmp = df.copy()
 
     for variable, dict_reemplazo in CAMBIO_HITO_2.items():
-        tmp[variable] = recodificar_variable(tmp[variable], dict_reemplazo)
-    
+        tmp[variable] = cambiar_valores_en_variable(
+            tmp[variable], dict_reemplazo)
+
     return tmp
+
 
 def preprocesar_y_recodificar_enunciado_dos(df):
     tmp = df.copy()
 
     tmp = tmp.replace(['nulidade', 'sem validade', 'zero'], np.nan)
     tmp = corregir_variables_numericas(tmp)
-    tmp = tmp.dropna()
     tmp = cambiar_vars_binarias_enunciado_dos(tmp)
     tmp = unir_codificacion_one_hot_vars_categoricas(tmp)
 
     return tmp
-    
-
