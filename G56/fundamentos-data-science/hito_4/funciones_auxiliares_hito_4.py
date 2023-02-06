a47ec442c9127e476100f9c30b93a12f38b10039
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-02-06 14:48:25 -0300

    Crea 3 modelos y unifica funcion para calc cross

--- G56/fundamentos-data-science/hito_4/funciones_auxiliares_hito_4.py
+++ G56/fundamentos-data-science/hito_4/funciones_auxiliares_hito_4.py
@@ -23,12 +23,12 @@ def separar_vector_objetivo_e_indep(df, vector_objetivo):
 
     return X, y
 
-def calcular_roc_cross_val(df, instancia_modelo, vector_objetivo, cantidad_folds):
+def calcular_roc_cross_val(regresor, cv, scoring, df, vector_objetivo):
     X, y = separar_vector_objetivo_e_indep(df, vector_objetivo)
     X_escalado = StandardScaler().fit_transform(X)
 
-    roc_acumulado = cross_val_score(instancia_modelo, X=X_escalado, y=y, cv=cantidad_folds, 
-                                    scoring='roc_auc')
+    roc_acumulado = cross_val_score(regresor, X=X_escalado, y=y, cv=cv, 
+                                    scoring=scoring)
     
     return roc_acumulado
 
