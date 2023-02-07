d0385f4a73d9fba3ffb13c487188bc659dca26ef
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-02-06 22:19:08 -0300

    Crea modelos para enunciado_2

--- G56/fundamentos-data-science/hito_4/funciones_auxiliares_hito_4.py
+++ G56/fundamentos-data-science/hito_4/funciones_auxiliares_hito_4.py
@@ -20,7 +20,7 @@ def separar_vector_objetivo_e_indep(df, vector_objetivo):
 
     return X, y
 
-def calcular_roc_cross_val(regresor, cv, scoring, df, vector_objetivo):
+def calcular_cross_val(regresor, cv, scoring, df, vector_objetivo):
     '''Función que calcula una métrica de desempeño para un cálculo de K-Folds de un regresor
     indicado por el usuario. Previo al cálculo de los K-Folds, las variables independientes se
     estandarizan.
@@ -49,8 +49,8 @@ def calcular_roc_cross_val(regresor, cv, scoring, df, vector_objetivo):
     X, y = separar_vector_objetivo_e_indep(df, vector_objetivo)
     X_escalado = StandardScaler().fit_transform(X)
 
-    roc_acumulado = cross_val_score(regresor, X=X_escalado, y=y, cv=cv, 
+    acumulado = cross_val_score(regresor, X=X_escalado, y=y, cv=cv, 
                                     scoring=scoring)
     
-    return roc_acumulado
+    return acumulado
 
