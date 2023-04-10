26eb1538d5cbc2acd4838d41b036429f9fffc478
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-04-10 19:20:24 -0400

    Cambia funciones helpers

--- G56/machine-learning/s5/c2/desafio_evaluado/helpers2.py
+++ G56/machine-learning/s5/c2/desafio_evaluado/helpers2.py
@@ -5,7 +5,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import classification_report, roc_curve, auc
+from sklearn.metrics import classification_report
+from sklearn.metrics import roc_curve, auc, f1_score, precision_score, recall_score
+
 
 def plot_classification_report(y_true, y_hat):
     """
@@ -20,38 +22,37 @@ def plot_classification_report(y_true, y_hat):
 
     """
     # process string and store in a list
-    report = classification_report(y_true, y_hat).split()
-    print(report)
-    # keep values
-    report = [i for i in report if i not in ['precision', 'recall', 'f1-score', 'support', 'avg',
-                                             'accuracy', 'macro', 'weighted']]
-    print(report)
-    # transfer to a DataFrame
-    report = pd.DataFrame(np.array(report).reshape(len(report) // 5, 5))
-    # asign columns labels
-    report.columns = ['idx', 'prec', 'rec', 'f1', 'n']
-    # preserve class labels
-    class_labels = report.iloc[:np.unique(y_true).shape[0]].pop('idx').apply(int)
-    # separate values
-    class_report = report.iloc[:np.unique(y_true).shape[0], 1:4]
-    # convert from str to float
-    class_report = class_report.applymap(float)
-    # convert to float average report
-    average_report = report.iloc[-1, 1: 4].apply(float)
+    f1_score_value_0 = f1_score(y_true=y_true, y_pred=y_hat, pos_label=0)
+    precision_score_value_0 = precision_score(y_true=y_true, y_pred=y_hat, pos_label=0)
+    recall_score_value_0 = recall_score(y_true=y_true, y_pred=y_hat, pos_label=0)
 
-    colors = ['dodgerblue', 'tomato', 'purple', 'orange']
+    f1_score_value_1 = f1_score(y_true=y_true, y_pred=y_hat, pos_label=1)
+    precision_score_value_1 = precision_score(y_true=y_true, y_pred=y_hat, pos_label=1)
+    recall_score_value_1 = recall_score(y_true=y_true, y_pred=y_hat, pos_label=1)
 
-    for i in class_labels:
-        plt.plot(class_report['prec'][i], [1], marker='x', color=colors[i])
-        plt.plot(class_report['rec'][i], [2], marker='x', color=colors[i])
-        plt.plot(class_report['f1'][i], [3], marker='x',color=colors[i], label=f'Class: {i}')
+    f1_score_value_avg = np.mean([f1_score_value_0, f1_score_value_1])
+    precision_score_value_avg = np.mean([precision_score_value_0, precision_score_value_1])
+    recall_score_value_avg = np.mean([recall_score_value_0, recall_score_value_1])
 
-    plt.scatter(average_report, [1, 2, 3], marker='o', color='forestgreen', label='Avg')
-    plt.yticks([1.0, 2.0, 3.0], ['Precision', 'Recall', 'f1-Score'])
+    colors = ["dodgerblue", "tomato", "purple", "orange"]
+
+    plt.yticks([1.0, 2.0, 3.0], ["Precision", "Recall", "f1"])
+    plt.xlim(0, 1)
+
+    plt.plot(precision_score_value_0, [1], marker="x", color=colors[0])
+    plt.plot(recall_score_value_0, [2], marker="x", color=colors[0], label=f"Class: {0}")
+    plt.plot(f1_score_value_0, [3], marker="x", color=colors[0])
+
+    plt.plot(precision_score_value_1, [1], marker="x", color=colors[1])
+    plt.plot(recall_score_value_1, [2], marker="x", color=colors[1], label=f"Class: {1}")
+    plt.plot(f1_score_value_1, [3], marker="x", color=colors[1])
+
+    plt.plot(precision_score_value_avg, [1], marker="o", color="forestgreen")
+    plt.plot(recall_score_value_avg, [2], marker="o", color="forestgreen", label="Avg")
+    plt.plot(f1_score_value_avg, [3], marker="o", color="forestgreen")
 
 
 def grid_plot_batch(df, cols, plot_type):
-
     """
     grid_plot_batch: Genera una grilla matplotlib para cada conjunto de variables.
 
@@ -66,15 +67,15 @@ def grid_plot_batch(df, cols, plot_type):
     """
     # calcular un aproximado a la cantidad de filas
     rows = int(np.ceil(df.shape[1] / cols))
-
     # para cada columna
+
     for index, (colname, serie) in enumerate(df.iteritems()):
         plt.subplot(rows, cols, index + 1)
-        plot_type(serie)
-        plt.title(colname)
+        plot_type(x=serie)
         plt.tight_layout()
 
-def identify_high_correlations(df, threshold=.7):
+
+def identify_high_correlations(df, threshold=0.7):
     """
     identify_high_correlations: Genera un reporte sobre las correlaciones existentes entre variables, condicional a un nivel arbitrario.
 
@@ -91,13 +92,14 @@ def identify_high_correlations(df, threshold=.7):
     # convertimos a long format
     tmp = pd.melt(tmp)
     # agregamos una columna extra que nos facilitará los cruces entre variables
-    tmp['var2'] = list(df.columns) * len(df.columns)
+    tmp["var2"] = list(df.columns) * len(df.columns)
     # reordenamos
-    tmp = tmp[['variable', 'var2', 'value']].dropna()
+    tmp = tmp[["variable", "var2", "value"]].dropna()
     # eliminamos valores duplicados
-    tmp = tmp[tmp['value'].duplicated()]
-    # eliminamos variables con valores de 1 
-    return tmp[tmp['value'] < 1.00]
+    tmp = tmp[tmp["value"].duplicated()]
+    # eliminamos variables con valores de 1
+    return tmp[tmp["value"] < 1.00]
+
 
 def plot_roc(model, y_true, X_test, model_label=None):
     """TODO: Docstring for plot_roc.
@@ -114,9 +116,9 @@ def plot_roc(model, y_true, X_test, model_label=None):
     store_auc = auc(false_positive_rates, true_positive_rate)
 
     if model_label is not None:
-        tmp_label = f'{model_label}: {round(store_auc, 3)}'
+        tmp_label = f"{model_label}: {round(store_auc, 3)}"
     else:
         tmp_label = None
     plt.plot(false_positive_rates, true_positive_rates, label=tmp_label)
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
