b5b65a8302ac610c88039905b5f91f69baf1e5d5
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-04-20 16:26:40 -0400

    Genera textos lower y sin stopwords

---
+++ G56/machine-learning/prueba/prueba_1/hito_2/funciones_auxiliares_hito_2.py
@@ -0,0 +1,27 @@
+import random
+
+random.seed(1)
+
+DICCIONARIO_REEMPLAZO = {
+    "worry": 0,
+    "happiness": 1,
+    "sadness": 0,
+    "love": 1,
+    "surprise": 1,
+    "fun": 1,
+    "relief": 1,
+    "hate": 0,
+    "empty": 0,
+    "enthusiasm": 1,
+    "boredom": 0,
+    "anger": 0,
+}
+
+
+def codificar_sentimientos(vector_objetivo):
+    vector_objetivo_codificado = vector_objetivo.replace(DICCIONARIO_REEMPLAZO)
+    neutrales = vector_objetivo_codificado[vector_objetivo_codificado == "neutral"]
+    neutrales = neutrales.apply(lambda x: random.choice([0, 1]))
+    vector_objetivo_codificado.iloc[neutrales.index] = neutrales
+
+    return vector_objetivo_codificado
