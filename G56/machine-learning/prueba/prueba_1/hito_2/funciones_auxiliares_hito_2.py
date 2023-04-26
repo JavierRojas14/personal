94560d77727dd3be2f5e08a965dd25ff3b4e4636
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-04-26 11:20:35 -0400

    Exporta funciones de analisis cv a archivo aux

--- G56/machine-learning/prueba/prueba_1/hito_2/funciones_auxiliares_hito_2.py
+++ G56/machine-learning/prueba/prueba_1/hito_2/funciones_auxiliares_hito_2.py
@@ -26,3 +26,43 @@ def codificar_sentimientos(vector_objetivo):
     vector_objetivo_codificado = vector_objetivo_codificado.astype(int)
 
     return vector_objetivo_codificado
+
+
+def resumir_resultados_grid_cv(diccionario_resultados):
+    df_resultados = pd.DataFrame(diccionario_resultados)
+    df_resultados["params_str"] = df_resultados["params"].astype(str)
+
+    return df_resultados
+
+
+def graficar_resultados_grid_cv(resultado_df):
+    sns.lineplot(data=resultado_df, x="params_str", y="mean_test_score", marker="o")
+    plt.tick_params(axis="x", labelrotation=90)
+
+
+def analizar_resultados_grid_cv(diccionario_resultados):
+    df_resultados = resumir_resultados_grid_cv(diccionario_resultados)
+    graficar_resultados_grid_cv(df_resultados)
+
+    return df_resultados
+
+
+def entrenar_y_obtener_desempeno_modelo_en_grilla(modelo_grilla, X_train, X_test, y_train, y_test):
+    modelo_grilla.fit(X_train, y_train)
+    print(f"\n\n--------------Resultados Conjunto de Entrenamiento-----------------")
+    print("Los resultados en la busqueda de hiperparametros son:")
+    resultados_grilla = analizar_resultados_grid_cv(modelo_grilla.cv_results_)
+    plt.show()
+    print(f"Los mejores parametros fueron: {modelo_grilla.best_params_}")
+    print(f"El mejor desempeno fue: {modelo_grilla.best_score_}")
+
+    print(f"\n\n--------------Resultados Conjunto de Validacion-----------------")
+    yhat = modelo_grilla.predict(X_test)
+    roc = roc_auc_score(y_test, yhat)
+    print("Los resultados en el conjunto de validacion son:")
+    print(classification_report(y_test, yhat))
+    print(f"El ROC fue de: {roc}")
+
+    print(f"---------------------------------------------------------------------")
+
+    return modelo_grilla, resultados_grilla
