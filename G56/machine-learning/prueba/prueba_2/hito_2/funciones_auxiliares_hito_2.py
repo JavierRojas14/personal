1a715c85b1116bdc9ac4f65e552c9a1efc451a9e
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-05-03 11:04:55 -0400

    Importa modulo necesario para funcionamiento

---
+++ G56/machine-learning/prueba/prueba_2/hito_2/funciones_auxiliares_hito_2.py
@@ -0,0 +1,22 @@
+import preproc_nyc_sqf as pre
+
+
+def preprocesar_vectores_objetivos(df):
+    df_suitable, _, _ = pre.create_suitable_dataframe(df)
+    df_suitable["arstmad"] = df_suitable["arstmade"].replace({"N": 0, "Y": 1})
+
+    mask = (
+        (df_suitable["pf_hands"] == "Y")
+        | (df_suitable["pf_wall"] == "Y")
+        | (df_suitable["pf_grnd"] == "Y")
+        | (df_suitable["pf_drwep"] == "Y")
+        | (df_suitable["pf_ptwep"] == "Y")
+        | (df_suitable["pf_baton"] == "Y")
+        | (df_suitable["pf_hcuff"] == "Y")
+        | (df_suitable["pf_pepsp"] == "Y")
+        | (df_suitable["pf_other"] == "Y")
+    )
+
+    df_suitable["violent"] = mask * 1
+
+    return df_suitable
