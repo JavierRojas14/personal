f4de9b3caa98cdfc71be24e4fc671e50634f34ce
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-05-12 11:19:17 -0400

    Inserta valores en tabla

--- G56/sql/s1/c1/desafio_guiado/desafio_guiado.sql
+++ G56/sql/s1/c1/desafio_guiado/desafio_guiado.sql
@@ -1,19 +1,19 @@
 CREATE DATABASE Biblioteca;
 
-CREATE TABLE Biblioteca.Libro(
+CREATE TABLE Libro(
     id_libro INT PRIMARY KEY,
     nombre_libro TEXT,
     autor TEXT,
     genero TEXT
 );
 
-INSERT INTO Biblioteca.Libro
-VALUES (1, "Sapo y Sepo", "Pepe", "Aventuras");
+INSERT INTO Libro
+VALUES (1, 'Sapo y Sepo', 'Pepe', 'Aventuras');
 
-INSERT INTO Biblioteca.Libro
-VALUES (2, "La Metamorfosis", "Juanito", "Misterio");
+INSERT INTO Libro
+VALUES (2, 'La Metamorfosis', 'Juanito', 'Misterio');
 
-CREATE TABLE Biblioteca.Prestamo(
+CREATE TABLE Prestamo(
     id_prestamo INT PRIMARY KEY,
     id_libro 
 )
