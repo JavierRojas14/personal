ebb29601cf4bf45b5bd8f98c1e6380398cc685f4
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-05-20 18:36:31 -0400

    termina query

--- G56/sql/s2/c1/desafio_guiado/desafio_guiado.sql
+++ G56/sql/s2/c1/desafio_guiado/desafio_guiado.sql
@@ -1,4 +1,6 @@
-SELECT nombre_artista
+SELECT *
+FROM cancion
+WHERE artista = (SELECT nombre_artista
 FROM artista
 WHERE nacionalidad = 'Estadounidense' AND fecha_de_nacimiento > '1992-01-01'
-LIMIT 1;
+LIMIT 1) AND numero_del_track = 4;
