afc2d5c8514d66f1eb93caf48d2af5bfbaef1682
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-05-22 14:59:09 -0400

    Empieza a aplicar la primera forma normal

--- G56/sql/s2/c2/desafio_evaluado/desafio_evaluado.sql
+++ G56/sql/s2/c2/desafio_evaluado/desafio_evaluado.sql
@@ -23,3 +23,50 @@ DELIMITER ','
 CSV HEADER;
 
 SELECT * FROM articulos;
+
+
+
+-- TABLA(codigo_producto,
+-- producto,
+-- local,
+-- precio,
+-- existencia,
+-- stock,
+-- ubicacion,
+-- numero_bodega,
+-- vendedor,
+-- rut_vendedor,
+-- numero_boleta,
+-- cantidad_vendida,
+-- rut_cliente,
+-- nombre_cliente)
+
+-- Antes de realizar cualquier normalizacion, se separara la tabla en 4 subtablas. Luego,
+-- estas 4 subtablas seran normalizadas
+
+-- Articulos(#codigo_producto,
+-- producto,
+-- local,
+-- precio,
+-- existencia,
+-- stock,
+-- ubicacion,
+-- numero_bodega)
+
+-- PersonalEmpresa(#rut_vendedor,
+-- vendedor)
+
+-- ClientesEmpresa(#rut_cliente,
+-- nombre_cliente)
+
+-- Transacciones(#numero_boleta, rut_vendedor, rut_cliente, codigo_producto, cantidad_vendida)
+
+-- 1era forma normal. Este cambio se realizara solo a la tabla PersonalEmpresa. Se obviaran
+las demas tablas, ya que cumplen la primera forma normal.
+
+-- PersonalEmpresa(#rut_vendedor, vendedor) pasara a PersonalEmpresa(#rut_vendedor, nombre_vendedor,
+sobrenombre_vendedor)
+
+
+
+
