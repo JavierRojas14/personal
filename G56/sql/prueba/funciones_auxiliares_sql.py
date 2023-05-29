642f3c38a16b1b57355457b9bdebde937ced0ab1
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-05-29 12:36:57 -0400

    Modulariza entrenamiento de modelos con varias var

--- G56/sql/prueba/funciones_auxiliares_sql.py
+++ G56/sql/prueba/funciones_auxiliares_sql.py
@@ -12,3 +12,19 @@ def insertar_csv_a_tabla_postgres(ruta_csv, nombre_tabla_destino, cursor):
 
         for row in reader:
             cursor.execute(query, row)
+
+
+def entrenar_modelos_en_tanda(vectores_objetivo, dict_modelos_a_entrenar, df_train):
+    resultados_modelos = {}
+
+    for vector_objetivo in vectores_objetivo:
+        for nombre_modelo, modelo in dict_modelos_a_entrenar.items():
+            X_train = df_train.drop(columns=vector_objetivo)
+            y_train = df_train[vector_objetivo]
+
+            modelo_entrenado = modelo.fit(X_train, y_train)
+            id_modelo = f"{nombre_modelo}_pred_{vector_objetivo}"
+
+            resultados_modelos[id_modelo] = modelo_entrenado
+
+    return resultados_modelos
